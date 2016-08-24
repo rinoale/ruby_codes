848f8b8f27796400959b45e78d234096990b0e79
rinoale <rinoale@hanmail.net>
2016-08-24 04:45:40 +0000

    extending class methods and include

--- module_test.rb
+++ module_test.rb
@@ -3,10 +3,17 @@ module Overwatch
     module Map
       def self.included(base)
         puts 'overwatch map included'
+        base.extend ClassMethods
       end
 
       def lobby
         puts 'this is public lobby'
+      end
+
+      module ClassMethods
+        def potion
+          puts 'this is class method, object'
+        end
       end
 
       private
@@ -14,6 +21,7 @@ module Overwatch
       def hanamura
         puts 'this is private map, hanamura'
       end
+
     end
   end
 end
@@ -35,4 +43,18 @@ lobby
 
 hanamura
 
-self.class.hanamura
+# self.class.hanamura
+
+self.class.potion
+
+class Competition
+  include Overwatch::Matching::Map
+end
+
+comp = Competition.new
+
+Competition.potion
+
+comp.lobby
+
+comp.hanamura
